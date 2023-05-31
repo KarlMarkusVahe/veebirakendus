ea92ecb78ccf7002ba63ce776c69c58f1be9edc8
KarlMarkusVahe <113974010+KarlMarkusVahe@users.noreply.github.com>
2023-05-31 09:49:33 +0300

    API päringud mudeli ja listiga

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import toode from "./controllers/toode";
+import toodelist from "./controllers/toodelist";
 
 const app: Express = express();
 
@@ -8,6 +9,7 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use('/', toode)
+app.use('/', toodelist)
 
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
